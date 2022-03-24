27a01f210026acab05da9fd9c95ec3645706a1b8
anshuman <anshuman@dezerv.in>
2022-03-24 20:32:54 +0530

    commiting test2 in branchx

---
+++ src/Test2.java
@@ -0,0 +1,12 @@
+
+public class Test2 {
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes") {
+			System.out.println("start eating");
+		}else {
+			System.out.println("no hungry");
+		}
+	}
+
+}
