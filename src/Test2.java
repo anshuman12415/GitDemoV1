38ec3a2018914d1ad3af138ad67d344279ea2c52
anshuman <anshuman@dezerv.in>
2022-03-24 20:58:06 +0530

    new checks added

--- src/Test2.java
+++ src/Test2.java
@@ -2,7 +2,7 @@
 public class Test2 {
 	public static void main(String[] args) {
 		String hungry="yes";
-		if(hungry=="yes") {
+		if(hungry=="yes" || hungry=="Y" || hungry=="y" ) {
 			System.out.println("start eating");
 		}else {
 			System.out.println("no hungry");
